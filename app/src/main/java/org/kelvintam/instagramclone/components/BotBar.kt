c66a39b0d01685cf39e51b916c8e1f916fe4041c
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-04-27 14:32:20 +0800

    Import vector icon for top & bottom bar

--- app/src/main/java/org/kelvintam/instagramclone/components/BotBar.kt
+++ app/src/main/java/org/kelvintam/instagramclone/components/BotBar.kt
@@ -1,23 +1,48 @@
 package org.kelvintam.instagramclone.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.kelvintam.instagramclone.R
 
 @Composable
 fun BotBar(){
-    Row(modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween) {
-        Text(text = "icon", textAlign = TextAlign.Center)
-        Text(text = "icon", textAlign = TextAlign.Center)
-        Text(text = "icon", textAlign = TextAlign.Center)
-        Text(text = "icon", textAlign = TextAlign.Center)
-        Text(text = "icon", textAlign = TextAlign.Center)
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.White)
+        .height(60.dp)
+        .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_home_24),
+            contentDescription = "Direct Message" // decorative element
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_search_24),
+            contentDescription = "Direct Message" // decorative element
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_add_box_24),
+            contentDescription = "Direct Message" // decorative element
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_notifications_24),
+            contentDescription = "Direct Message" // decorative element
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_person_24),
+            contentDescription = "Direct Message" // decorative element
+        )
     }
 }
 
--- app/src/main/java/org/kelvintam/instagramclone/components/Top.kt
+++ app/src/main/java/org/kelvintam/instagramclone/components/Top.kt
@@ -1,25 +1,45 @@
 package org.kelvintam.instagramclone.components
 
+import org.kelvintam.instagramclone.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Top(){
-    Row(modifier = Modifier.fillMaxWidth().background(Color.Magenta),horizontalArrangement = Arrangement.SpaceBetween,) {
-        Text(text = "camera icon")
-        Text(text = "message icon")
+fun Top() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .height(50.dp)
+            .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+//        Text(text = "camera icon")
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_photo_camera_24),
+            contentDescription = "Add a story" // decorative element
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_send_24),
+            contentDescription = "Direct Message" // decorative element
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TopPreview(){
+fun TopPreview() {
     Top()
 }
