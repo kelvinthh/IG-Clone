73fd682b73f1e3043bda36e587fe1ded6147da20
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2022-04-28 08:44:39 +0800

    SwipeRefresh

--- app/src/main/java/org/kelvintam/instagramclone/Feed.kt
+++ app/src/main/java/org/kelvintam/instagramclone/Feed.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
 import org.kelvintam.instagramclone.components.BotBar
 import org.kelvintam.instagramclone.components.Posts
 import org.kelvintam.instagramclone.components.Stories
@@ -25,16 +28,28 @@ fun Feed() {
         topBar = { Top() },
         bottomBar = { BotBar() }
     ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-
+        var refreshing by remember { mutableStateOf(false) }
+        LaunchedEffect(refreshing) {
+            if (refreshing) {
+                delay(2000)
+                refreshing = false
+            }
+        }
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(isRefreshing = refreshing),
+            onRefresh = { refreshing = true }
         ) {
-            item { Stories() }
-            items(100) { index ->
-                Posts(index)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+
+            ) {
+                item { Stories() }
+                items(100) { index ->
+                    Posts(index)
+                }
             }
         }
     }
